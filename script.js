3877dd9f7d560b6a0a0d45c4f640cdadd7b6663d
harisshafi10 <harisshafi01@gmail.com>
2025-01-18 23:08:04 +0530

    updated javascript

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-window.addEventListener('keydown',function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    if(!audio) return;
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
     audio.currentTime = 0;
-    audio.play()
-    console.log(e.keyCode)
-    key.classList.add('playing')
-})
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return;
-    console.log(e.propertyName)
+    audio.play();
+    console.log(e.keyCode);
+    key.classList.add('playing');
+});
 
-    this.classList.remove('playing')
+function playsound(e) {
+    const keyCode = e.type === 'click' ? e.currentTarget.getAttribute('data-key') : e.keyCode;
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    console.log(e.propertyName);
+    this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', playsound);   
+});
